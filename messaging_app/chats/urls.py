ec8ad218d572fecb1d98f18f69942c9871a73c8a
Chimdinyelum <chyjohn777@gmail.com>
2025-09-21 21:51:23 +0100

    Edit 10: Week 4: Building Robust APIs

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,12 +1,21 @@
 # urls.py
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_extensions.routers import NestedRouterMixin
 from chats import views
 
-router = routers.DefaultRouter()
-router.register(r'conversations', views.ConversationViewSet,
-                basename='conversation')
-router.register(r'messages', views.MessageViewSet, basename='message')
+# Create NestedDefaultRouter
+
+
+class NestedDefaultRouter(NestedRouterMixin, routers.DefaultRouter):
+    pass
+
+
+router = NestedDefaultRouter()
+conversations_router = router.register(r'conversations', views.ConversationViewSet,
+                                       basename='conversation')
+conversations_router.register(r'messages', views.MessageViewSet,
+                              basename='conversation-messages', parents_query_lookups=['conversation'])
 
 urlpatterns = [
     path('api/', include(router.urls)),
--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('chats.urls')),  # Normal include
-    path('api/', include('chats.urls')),  # Explicit api/ path for grading
+    path('', include('chats.urls')),
+    path('api/', include('chats.urls')),  # Explicit api/ path
+    # Add api-auth for DRF browsable API
+    path('api-auth/', include('rest_framework.urls')),
 ]
