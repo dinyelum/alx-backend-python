689cc68547d973897c124c97c087962984758d29
Chimdinyelum <chyjohn777@gmail.com>
2025-09-21 14:50:06 +0100

    Edit 7: Week 4: Building Robust APIs

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -3,11 +3,11 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from chats import views
 
-router = DefaultRouter()
-router.register(r'conversations', views.ConversationViewSet,
-                basename='conversation')
-router.register(r'messages', views.MessageViewSet, basename='message')
+routers = DefaultRouter()
+routers.register(r'conversations', views.ConversationViewSet,
+                 basename='conversation')
+routers.register(r'messages', views.MessageViewSet, basename='message')
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('api/', include(routers.urls)),
 ]
